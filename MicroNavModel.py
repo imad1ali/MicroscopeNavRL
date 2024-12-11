2d2a436599a7189a280375d98bf3ff376aa7d42f
imad1ali <imadmustafaali@gmail.com>
2024-12-11 16:05:25 +0000

    added new class for functions in the roi

---
+++ MicroNavModel.py
@@ -0,0 +1,8 @@
+from environment_class import Environment
+
+class Model(self):
+    
+    def __innit__(self):
+        
+        pass
+    pass
---
+++ roi_class.py
@@ -0,0 +1,13 @@
+from environment_class import Environment
+
+"""
+    Ali's code
+    
+"""
+class roi(Environment):
+    
+    def get_roi_cell_count(self):
+        
+        #image = self.roi
+        pass
+    
