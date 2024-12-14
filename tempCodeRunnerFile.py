d898062d5034be285e63a143e3d992c3a8a70f31
Ali Adldoust <aliadldoust1998@gmail.com>
2024-12-14 12:10:27 +0000

    new_detection

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-2
+        # ver = (cv2.__version__).split('.')
+        # if int (ver[0]) < 3:
+        #     self.detector = cv2.SimpleBlobDetector(params)
+        # else:
+        #     self.detector = cv2.SimpleBlobDetector_create(params)
