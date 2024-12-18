fb39714b59a99a5d2ca815f45a66995afb5ace56
imad1ali <imadmustafaali@gmail.com>
2024-12-18 21:02:37 +0000

    model v1

--- MicroNavModel.py
+++ MicroNavModel.py
@@ -2,7 +2,19 @@ from environment_class import Environment
 
 class Model(self):
     
-    def __innit__(self):
+    def __init__(self, env_path):
         
-        pass
+        environment = Environment(env_path)
+        
+        self.state = {
+            "roi_center" : environment.roi_center,
+            "cell_count" : None,
+            "roi_cell_count" : None
+        }
+        
+        
     pass
+
+
+env_path = r'C:\Users\imad\Desktop\KRR\MicroscopeNavRL\images\A172_Phase_A7_1_00d00h00m_1.tif'
+model1 = Model(env_path)
