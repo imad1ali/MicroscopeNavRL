5f1229e85deaa4a6449aca5441bf0fbcf8619f8a
imad1ali <imadmustafaali@gmail.com>
2024-12-18 21:02:10 +0000

    cell count now in new function

--- environment_class.py
+++ environment_class.py
@@ -22,6 +22,7 @@ class Environment:
         
         # obtain the region of interets in a new image (variable)
         self.roi = self.sample_space[self.roi_y[0]:self.roi_y[1], self.roi_x[0]:self.roi_x[1]]
+        self.roi_8bit = (self.roi / np.max(self.roi) * 255).astype('uint8')
 
     def move(self, move_x, move_y):
         # decesions go here, they will move the region of interest
@@ -49,20 +50,37 @@ class Environment:
         cv2.imshow('sample_space_8bit', self.sample_space_8bit)
         cv2.waitKey(100)  # Adds a small delay for smooth visualization
         
+    def get_roi_cell_count(self):
+        im_with_keypoints = cv2.cvtColor(self.roi_8bit, cv2.COLOR_GRAY2BGR)
+        contours, _ = cv2.findContours(self.roi_8bit, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        print(f"Number of blobs detected: {len(contours)}")
+        # Loop through each contour to draw a symbol (e.g., circle) at the centroid
+        for contour in contours:
+            M = cv2.moments(contour)
+            # Calculate the centroid (center) of the contour
+            if M["m00"] != 0:  # To avoid division by zero
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+    
+                cv2.circle(im_with_keypoints, (cX, cY), 10, (0, 0, 255), -1)  # Red circle with radius 10
+        cv2.imshow("Detected Cells in ROI", im_with_keypoints)
+        
            
 #------------------------------------------------------------------------------------------
         
-environment1 = Environment(r"D:\OneDrive - MMU\Desktop\Knowledge Representation and Reasoning\MicroscopeNavRL\images\mask\A172_Phase_C7_1_00d00h00m_1_mask.tif")
+environment1 = Environment(r"images\mask\A172_Phase_C7_1_00d00h00m_1_mask.tif")
 
 # move right 50 pixels
 for i in range(100):
     environment1.move(1,0)
     print("moved")
+    environment1.get_roi_cell_count()
     environment1.update_display()
     
 # move down 50 pixels
 for i in range(100):
     environment1.move(0,1)
+    environment1.get_roi_cell_count()
     print("moved")
     environment1.update_display()
     
